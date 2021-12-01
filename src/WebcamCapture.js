f09b89c2cdffc7c8b58dd2298ab8ebb5956cabe6
furkandindar <fdindar1@binghamton.edu>
2021-12-01 14:50:02 +0300

    add capture image functionality

--- src/WebcamCapture.js
+++ src/WebcamCapture.js
@@ -1,5 +1,6 @@
-import React, { useRef } from 'react'
+import React, { useRef, useCallback, useState } from 'react'
 import Webcam from 'react-webcam'
+import RadioButtonUncheckedIcon from '@mui/icons-material/RadioButtonUnchecked';
 
 const videoConstraints = {
     facingMode: "environment",
@@ -7,14 +8,29 @@ const videoConstraints = {
 
 function WebcamCapture() {
     const webcamRef = useRef(null);
+    const [image, setImage] = useState(null);
+
+    const capture = useCallback(() => {
+        const imageSource = webcamRef.current.getScreenshot();
+        console.log(imageSource);
+        setImage(imageSource);
+    }, [webcamRef])
+    
     return (
-        <div>
+        <div className="webcamCapture">
             <Webcam
                 ref={webcamRef}
                 audio={false}
                 screenshotFormat="image/jpeg"
                 videoConstraints={videoConstraints}
             />
+
+            <RadioButtonUncheckedIcon
+                className="webcamCapture__button"
+                onClick = {capture}
+                fontSize = "large"
+            />
+            <img src={image}></img>
         </div>
     )
 }

6776470f4b45f0e57369c728c401a859449101a7
furkandindar <fdindar1@binghamton.edu>
2021-12-01 14:07:11 +0300

    show 3d model

--- src/App.js
+++ src/App.js
@@ -7,12 +7,12 @@ import { Grid } from '@mui/material';
 
 
 const Model = () => {
-  const glb = useLoader(GLTFLoader, "./Ring.glb");
+  const glb = useLoader(GLTFLoader, "https://3dfoodmodel-modelviewer.s3.amazonaws.com/assets/Bolle/Nevada_Blue/BolleNevada_Blue_v1.glb");
   const ref = useRef();
 
   useFrame((state, delta) => {
-    ref.current.position.x = (landmark_x - 0.5)*10;
-    ref.current.position.y = -(landmark_y - 0.5)*7.5;
+    ref.current.position.x = (landmark_x - 0.5)*7.5;
+    ref.current.position.y = -(landmark_y - 0.5)*10;
   })
 
   return (
@@ -32,7 +32,11 @@ function App() {
   const canvasRef = useRef(null);
 
   function onResults(results){
-    console.log(results);
+    canvasRef.current.width = imgRef.current.width;
+    canvasRef.current.height = imgRef.current.height;
+
+    console.log(imgRef);
+    console.log(canvasRef);
     if (results.multiHandLandmarks) {
       for (const landmarks of results.multiHandLandmarks) {
         if (landmarks[14].x !== "undefined") {
@@ -77,8 +81,15 @@ function App() {
     <Grid container direction="column">
       <Grid item container direction="row" xs={12} sm={12} md={12} lg={12} xl={12}>
         <Grid item xs={0} sm={2} md={3} lg={3} xl={4}></Grid>
-        <Grid item xs={12} sm={8} md={6} lg={6} xl={4}><img ref={imgRef} src="https://thumbs.dreamstime.com/b/back-side-woman-hand-showing-five-count-36194782.jpg" alt="hand"/></Grid>
-        <Grid item item xs={0} sm={2} md={3} lg={3} xl={4}></Grid>
+        <Grid item xs={12} sm={8} md={6} lg={6} xl={4}>
+              <img ref={imgRef} src="woman_hand.jpeg" style={{width:"480px", height:"640px", position:"absolute"}} alt="hand"/>
+              <Canvas style={{width:"480px", height:"640px", position:"absolute"}} ref={canvasRef}>
+                <Suspense fallback={null}>
+                <Model position={[0,0,-3]}></Model>
+                </Suspense>
+              </Canvas>
+        </Grid>
+        <Grid item xs={0} sm={2} md={3} lg={3} xl={4}></Grid>
       </Grid>
     </Grid>
   );

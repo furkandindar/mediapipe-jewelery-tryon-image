5ffad6efa578e3651308dd86ba4375658908f12f
furkandindar <fdindar1@binghamton.edu>
2021-12-03 15:36:40 +0300

    fix preview close button

--- src/App.js
+++ src/App.js
@@ -1,31 +1,7 @@
 import './App.css';
-import {Hands} from "@mediapipe/hands";
-import {useRef, useEffect, Suspense} from "react";
-import {useLoader, useFrame, Canvas} from "@react-three/fiber";
-import {GLTFLoader} from "three/examples/jsm/loaders/GLTFLoader";
 import { Grid } from '@mui/material';
 import WebcamCapture from "./WebcamCapture";
 
-
-const Model = () => {
-  const glb = useLoader(GLTFLoader, "https://3dfoodmodel-modelviewer.s3.amazonaws.com/assets/Bolle/Nevada_Blue/BolleNevada_Blue_v1.glb");
-  const ref = useRef();
-
-  useFrame((state, delta) => {
-    ref.current.position.x = (landmark_x - 0.5)*7.5;
-    ref.current.position.y = -(landmark_y - 0.5)*10;
-  })
-
-  return (
-    <>
-      <primitive ref={ref} object={glb.scene} scale={5}></primitive>
-    </>
-  );
-
-};
-
-var landmark_x = -100;
-var landmark_y = -100;
 
 function App() {
 
@@ -71,14 +47,6 @@ function App() {
 
 
   return (
-    // <>
-    //   <img ref={imgRef} src="./woman_hand.jpeg" alt="hand"/>
-    //   <Canvas ref={canvasRef}>
-    //     <Suspense fallback={null}>
-    //     <Model position={[0,0,-3]}></Model>
-    //     </Suspense>
-    //   </Canvas>
-    // </>
     <Grid container direction="column">
       <Grid item container direction="row" xs={12} sm={12} md={12} lg={12} xl={12}>
         <Grid item xs={0} sm={2} md={3} lg={3} xl={4}></Grid>
--- src/WebcamCapture.js
+++ src/WebcamCapture.js
@@ -1,8 +1,31 @@
-import React, { useRef, useCallback, useState } from 'react'
+import React, { useRef, useCallback, useState, useEffect, Suspense } from 'react'
 import Webcam from 'react-webcam'
 import RadioButtonUncheckedIcon from '@mui/icons-material/RadioButtonUnchecked';
 import CloseIcon from '@mui/icons-material/Close';
 import "./WebcamCapture.css";
+import {Hands} from "@mediapipe/hands";
+import {useLoader, useFrame, Canvas} from "@react-three/fiber";
+import {GLTFLoader} from "three/examples/jsm/loaders/GLTFLoader";
+
+const Model = () => {
+    const glb = useLoader(GLTFLoader, "https://3dfoodmodel-modelviewer.s3.amazonaws.com/assets/Bolle/Nevada_Blue/BolleNevada_Blue_v1.glb");
+    const ref = useRef();
+  
+    useFrame((state, delta) => {
+      //ref.current.position.x = (landmark_x - 0.5)*7.5;
+      //ref.current.position.y = -(landmark_y - 0.5)*10;
+    })
+  
+    return (
+      <>
+        <primitive ref={ref} object={glb.scene} scale={5}></primitive>
+      </>
+    );
+  
+  };
+  
+  var landmark_x = -100;
+  var landmark_y = -100;
 
 const videoConstraints = {
     facingMode: "environment",
@@ -11,15 +34,21 @@ const videoConstraints = {
 function WebcamCapture() {
     const webcamRef = useRef(null);
     const [image, setImage] = useState(null);
+    const imgRef = useRef(null);
+    const canvasRef = useRef(null);
+    console.log(imgRef);
 
     const capture = useCallback(() => {
         const imageSource = webcamRef.current.getScreenshot();
         setImage(imageSource);
-    }, [webcamRef])
+        console.log(imgRef);
+    }, [webcamRef]);
 
     const resetImg = useCallback(() => {
         setImage(null);
-    }, [webcamRef])
+    }, [webcamRef]);
+
+
 
     return (
         <div>
@@ -37,7 +66,12 @@ function WebcamCapture() {
                 />
             </div>
             <div className={`preview ${image ? "" : "hide"}`}>
-                <img src={image}></img>
+                <img ref={imgRef} src={image}></img>
+                {/* <Canvas ref={canvasRef}>
+                    <Suspense fallback={null}>
+                    <Model position={[0,0,-3]}></Model>
+                    </Suspense>
+                </Canvas>  */}
                 <CloseIcon 
                     className="webcamCapture__CloseButton"
                     onClick= {resetImg}

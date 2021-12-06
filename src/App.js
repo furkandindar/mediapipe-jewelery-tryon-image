bba984fc37742ef2cda1588e6d9a8007d1645934
furkandindar <fdindar1@binghamton.edu>
2021-12-06 21:11:03 +0300

    code cleanup

--- src/App.js
+++ src/App.js
@@ -5,47 +5,6 @@ import WebcamCapture from "./WebcamCapture";
 
 function App() {
 
-  // const imgRef = useRef(null);
-  // const canvasRef = useRef(null);
-
-  // function onResults(results){
-  //   canvasRef.current.width = imgRef.current.width;
-  //   canvasRef.current.height = imgRef.current.height;
-
-  //   console.log(imgRef);
-  //   console.log(canvasRef);
-  //   if (results.multiHandLandmarks) {
-  //     for (const landmarks of results.multiHandLandmarks) {
-  //       if (landmarks[14].x !== "undefined") {
-  //         landmark_x = (landmarks[14].x + landmarks[13].x)/2;
-  //         landmark_y = (landmarks[14].y + landmarks[13].y)/2;
-  //         //landmark_z = (landmarks[14].z + landmarks[13].z)/2;
-  //         //scale = landmarks[13].y - landmarks[14].y;
-  //       }
-  //     }
-  //   }
-  // }
-
-  // useEffect(() => {
-  //   const hands = new Hands({
-  //     locateFile:(file) =>{
-  //       return `https://cdn.jsdelivr.net/npm/@mediapipe/hands/${file}`;
-  //     }
-  //   });
-
-  //   hands.setOptions({
-  //     maxNumHands: 2,
-  //     modelComplexity: 1,
-  //     minDetectionConfidence: 0.5,
-  //     minTrackingConfidence: 0.5,
-  //   });
-
-  //   hands.onResults(onResults);
-
-  //   hands.send({image: imgRef.current});
-  // });
-
-
   return (
         <Grid container>
           <Grid item xs={12} sm={12}>

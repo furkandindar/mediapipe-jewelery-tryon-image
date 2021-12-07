edb9cb7896c98486c92ed691d68ef355b6ead8d3
furkandindar <fdindar1@binghamton.edu>
2021-12-07 15:54:41 +0300

    add overflow-x hidden

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import WebcamCapture from "./WebcamCapture";
 function App() {
 
   return (
-        <Grid container>
+        <Grid container style={{overflowX:"hidden"}}>
           <Grid item xs={12} sm={12} lg={6} xl={4}>
             <WebcamCapture/>
           </Grid>

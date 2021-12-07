17f124c5241657de27fb50af6ee47c780d4dc3d7
furkandindar <fdindar1@binghamton.edu>
2021-12-07 18:25:05 +0300

    add 100vh height to grid container

--- src/App.js
+++ src/App.js
@@ -6,10 +6,12 @@ import WebcamCapture from "./WebcamCapture";
 function App() {
 
   return (
-          <Grid container>
+          <Grid container style={{background:"red", height:"100vh"}}>
+          <Grid item xs={0} sm={0} lg={3} xl={4}></Grid>
           <Grid item xs={12} sm={12} lg={6} xl={4} style={{overflowX:"hidden", background:"green"}}>
             <WebcamCapture/>
           </Grid>
+          <Grid item xs={0} sm={0} lg={3} xl={4}></Grid>
         </Grid>
   );
 }

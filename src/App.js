6d374dd907bcc42e4c99517adc24388ecdc73544
furkandindar <fdindar1@binghamton.edu>
2021-12-07 15:47:20 +0300

    remove overflow:hidden

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import WebcamCapture from "./WebcamCapture";
 function App() {
 
   return (
-        <Grid container style={{overflow:"hidden"}}>
+        <Grid container>
           <Grid item xs={12} sm={12} lg={6} xl={4}>
             <WebcamCapture/>
           </Grid>
